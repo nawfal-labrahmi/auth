7d25cd4339adb6d3259f64c5d559fcd4ae4830ff
Nawfal Labrahmi <nawfal.labrahmi@gmail.com>
2018-07-22 00:03:36 +0200

    Conditional rendering based on auth processing result

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '' };
+  state = { email: '', password: '', error: '', loading: false };
 
   onButtonPress() {
     const { email, password } = this.state;
+    this.setState({ error: '', loading: true });
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({ error: 'Authentication failed.' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
       });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
+
+  onLoginFail() {
+    this.setState({
+      error: 'Authentication failed.',
+      loading: false,
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size='small' />;
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
   }
 
   render() {
@@ -28,7 +56,6 @@ class LoginForm extends Component {
             label='Email'
           />
         </CardSection>
-
         <CardSection>
           <Input
             secureTextEntry
@@ -38,15 +65,11 @@ class LoginForm extends Component {
             label='Password'
           />
         </CardSection>
-
         <Text style={styles.errorTextStyle}>
           {this.state.error}
         </Text>
-
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
