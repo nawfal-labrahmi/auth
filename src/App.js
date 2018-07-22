76d4d401e4b139d29edd5efe6d49b7f7541c55a9
Nawfal Labrahmi <nawfal.labrahmi@gmail.com>
2018-07-22 02:25:16 +0200

    More conditional rendering based on logged in state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCzxV2HmDxrBfTr6oOIEUALUPjVpy3oox4',
@@ -15,12 +17,33 @@ class App extends Component {
       storageBucket: 'auth-625ef.appspot.com',
       messagingSenderId: '862797576541'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true : return (
+        <Button>
+          Log out
+        </Button>
+      );
+      case false : return (<LoginForm />);
+      default : return (<Spinner />);
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
